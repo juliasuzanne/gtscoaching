313e3ae93039e7212ec3edcc53532c5b5c55525b
Julia Grimes <julia.s.grimes@gmail.com>
2024-05-17 17:51:32 -0400

    change cv and linkedin links to change format on mobile vs desktop

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,20 +1,25 @@
 export function Footer() {
   return (
     <div>
+      <div className="hideOnComp">
+        <ul className="footer">
+          <li className="nav-item" id="nav-item">
+            <a className="nav-link active" aria-current="page" href="/SJKResumeGTS.pdf" download>
+              CV
+            </a>
+          </li>
+          <li className="nav-item" id="nav-item">
+            <a className="nav-link">•</a>
+          </li>
+          <li className="nav-item" id="nav-item">
+            <a className="nav-link" href="https://www.linkedin.com/in/sarakontalonis/" target="_blank">
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </div>
+
       <ul className="footer">
-        <li className="nav-item" id="nav-item">
-          <a className="nav-link active" aria-current="page" href="/SJKResumeGTS.pdf" download>
-            CV
-          </a>
-        </li>
-        <li className="nav-item" id="nav-item">
-          <a className="nav-link">•</a>
-        </li>
-        <li className="nav-item" id="nav-item">
-          <a className="nav-link" href="https://www.linkedin.com/in/sarakontalonis/" target="_blank">
-            LinkedIn
-          </a>
-        </li>
         <li className="nav-item">
           <a className="nav-link">©2024 Growth Talent Solutions</a>
         </li>
--- src/Logo.jsx
+++ src/Logo.jsx
@@ -8,6 +8,18 @@ export function Logo() {
           src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1710951877/logonooutline_wkak3o.png"
         ></img>
       </a>
+      <ul className="gtsLogoList">
+        <li id="nav-item-list">
+          <a className="nav-link" href="https://www.linkedin.com/in/sarakontalonis/" target="_blank">
+            LinkedIn
+          </a>
+        </li>
+        <li id="nav-item-list">
+          <a className="nav-link active" href="/SJKResumeGTS.pdf" download>
+            CV
+          </a>
+        </li>
+      </ul>
       <p></p>
       <p></p>
     </div>
